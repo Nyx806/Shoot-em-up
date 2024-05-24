f985964ed7b89a15ccc36fec5220b5076955b6c0
nexus806 <antonin.merlomeyffren@ynov.com>
2024-05-24 09:42:56 +0200

    pool patern shoot
    création d'un tableau qui continent les projectiles a tiré et ajout du système de tire

--- src/class/Game.class.js
+++ src/class/Game.class.js
@@ -1,7 +1,8 @@
 
 import { Player } from './Player.class.js';
+import { Projectile } from './Projectile.class.js';
 
-export class Game {
+export  class Game {
 
     constructor(canvas) {
         this.canvas = canvas;  
@@ -9,20 +10,26 @@ export class Game {
         this.height = this.canvas.height;
         this.keys = [];
         this.Player = new Player(this);
+        
+        
+
 
         window.addEventListener('keydown', e =>{
             if (this.keys.indexOf(e.key) === -1) this.keys.push(e.key);
-            console.log(this.keys);
+            console.log(e.key)
+            if(e.key ==='a') this.Player.shoot();
         })
         window.addEventListener('keyup', e =>{
            const index = this.keys.indexOf(e.key);
            if (index >-1) this.keys.splice(index, 1);
             console.log(this.keys);
+            
         })
 
     }
     render(context){
         this.Player.draw(context);
         this.Player.update();
+        this.Player.showProjectiles(context);
     }
 }
